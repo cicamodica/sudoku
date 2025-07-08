0771bd727d3f3b870e307ab134147c850f762b01
Maria Cecilia Caruzzo Modica <ceciliacmodica@gmail.com>
2025-07-08 20:22:55 -0300

    Adicionando página do jogo

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function teste() {
-    
-}
+document.getElementById("btn-jogar").addEventListener("click", () => {
+  const dificuldade = document.getElementById("dificuldade").value;
+  localStorage.setItem("dificuldadeSelecionada", dificuldade);
+  window.location.href = "jogo.html";
+});
+
+
+ window.addEventListener('DOMContentLoaded', () => {
+        const dificuldade = localStorage.getItem('dificuldadeSelecionada');
+        console.log('Dificuldade selecionada:', dificuldade);
+
+        // Exemplo: Muda o título ou inicia lógica de jogo com base na dificuldade
+        const titulo = document.getElementById('titulo-jogo');
+        titulo.textContent = `Sudoku - ${dificuldade.charAt(0).toUpperCase() + dificuldade.slice(1)}`;
+      });
